484cde12d5886cd5a84df16500aaa9df7e8d395d
Ilya Outcast <outcastik89@gmail.ru>
2018-03-01 16:06:19 +0300

    cssnano zindex false

--- form/gulpfile.js
+++ form/gulpfile.js
@@ -27,7 +27,7 @@ gulp.task('sass', function () {
         .pipe(autoprefixer({
             browsers: ['last 2 version', 'safari 5', 'ie 9', 'opera 12.1', 'ios 6', 'android 4']
         }))
-        .pipe(production(cssnano()))
+        .pipe(production(cssnano({ zindex: false })))
         .pipe(development(sourcemaps.write()))
         .pipe(rename('paybear.css'))
         .pipe(gulp.dest(config.dist))
